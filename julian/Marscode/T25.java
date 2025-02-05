0a7fdcb7cafb304108f0416ee2c54892cb6a8945
JulianChu <1664553649@qq.com>
2025-02-05 21:01:13 +0000

    modified:   .DS_Store 	modified:   julian/Marscode/T25.java 	modified:   julian/Marscode/T26.java

--- julian/Marscode/T25.java
+++ julian/Marscode/T25.java
@@ -1,16 +1,33 @@
 package Marscode;
-import java.util.*;
 
-public class T16 {
-    public static int solution(int n, int[] array) {
+public class T25 {
+    public static int solution(String dna1, String dna2) {
+        int m = dna1.length();
+        // System.out.println(m);
+        int n = dna2.length();
 
+        int[][] dp = new int[m + 1][n + 1];
 
-        return 0;
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = j;
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                int cost = dna1.charAt(i - 1) == dna2.charAt(j - 1) ? 0 : 1;
+                dp[i][j] = Math.min(Math.min(dp[i -1][j] + 1, dp[i][j - 1] + 1), dp[i - 1][j - 1] + cost);
+            }
+        }
+
+        return dp[m][n];
     }
 
     public static void main(String[] args) {
-        // Add your test cases here
-
-        System.out.println(solution(5, new int[]{1, 2, 3, 4, 5}) == 9);
+        //  You can add more test cases here
+        System.out.println(solution("AGCTTAGC", "AGCTAGCT") == 2);
+        System.out.println(solution("AGCCGAGC", "GCTAGCT") == 4);
     }
 }
--- julian/Marscode/T26.java
+++ julian/Marscode/T26.java
@@ -1,4 +1,47 @@
 package Marscode;
 
 public class T26 {
+    public static boolean compare(String a, String b, int i) {
+        for (int j = 0; j < Math.min(b.length(), a.length()); j++) {
+            if (b.charAt(j) > a.charAt(j + i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public static int solution(int a, int b) {
+        String strA = String.valueOf(a);
+        String strB = String.valueOf(b);
+        StringBuilder result = new StringBuilder();
+        boolean flag = true;
+
+        if (b == 0) {
+            return a * 10;
+        }
+
+        for (int i = 0; i < strA.length(); i++) {
+            if (compare(strA, strB, i)) {
+                if (flag) {
+                    result.append(strB);
+                    flag = false;
+                }
+            }
+            result.append(strA.charAt(i));
+        }
+
+        if (result.length() == strA.length()) {
+            result.append(strB);
+        }
+
+        return Integer.parseInt(result.toString());
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(76543, 4) == 765443);
+        System.out.println(solution(1, 0) == 10);
+        System.out.println(solution(44, 5) == 544);
+        System.out.println(solution(666, 6) == 6666);
+    }
 }
